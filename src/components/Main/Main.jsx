75ba4b4009d9de6cb5f06864cc4a6a856cd68509
chochelius <chochelius@gmail.com>
2024-12-31 01:01:12 +0000

    host

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,16 @@ import React from 'react'
 
 function Main() {
     return (
-        <div>
-            <div>
-                <h1>Erradica</h1>
+        <>
+            <div className="row">
+                <div className="col-4 px-1">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil ratione at cum accusantium beatae doloremque debitis in fugiat?
+                </div>
+                <div className="col-8 px-5">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim repellendus dolores quidem, provident quisquam sint asperiores omnis doloribus minus deserunt veritatis laboriosam vitae aut consectetur debitis. Obcaecati tempora reprehenderit animi soluta vitae id eos iure a non veniam est possimus commodi praesentium assumenda culpa quia modi dolorem,
+                </div>
             </div>
-            <p className="read-the-docs">
-                Cooperativa de trabajo
-            </p>
-            <h2>Blog</h2>
-        </div>
+        </>
 
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
       <nav className="navbar bg-body-tertiary fixed-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="_blank">[LOGO]Erradica Cooperativa</a>
+          <a className="navbar-brand" href="_blank">[LOGO] Erradica Cooperativa</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
