9984faf0439262ab9db5daca20b2094b247a6552
chochelius <chochelius@gmail.com>
2025-02-28 12:37:42 -0300

    logo update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { Suspense, lazy } from 'react'; // Combine imports
-import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import { Suspense, lazy } from "react"; // Combine imports
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const Main = lazy(() => {
   // No need for delayForDemo in production
-  return delayForDemo(1500).then(() => import('./components/Main/Main'));
+  return delayForDemo(1500).then(() => import("./components/Main/Main"));
 });
 
 function App() {
   return (
     <>
       <Navbar />
-      <Suspense fallback={BigSpinner()}> {/* Use component directly */}
+      <Suspense fallback={BigSpinner()}>
+        {" "}
+        {/* Use component directly */}
         <Main />
-      </Suspense >
+      </Suspense>
       <Footer />
     </>
   );
 }
 
 function BigSpinner() {
-  return <h2>🌀 Loading...</h2>;
+  return (
+    <div>
+      <img
+        src="https://pbe.choche.bond/api/files/pbc_3607937828/i85z6tb8lrg48vz/untitled_project_8_1gn72upmug.png?token="
+        alt=""
+        style={{ width: "7vh" }}
+      />{" "}
+      <h2> Cargando...</h2>
+    </div>
+  );
 }
 
 const delayForDemo = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
